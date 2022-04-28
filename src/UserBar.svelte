7338bcde360a635ae67ec5e2ab927b9757cdf5f3
aetherised <aetherised@gmail.com>
2022-04-28 16:21:33 -0400

    adjust user bar

--- src/UserBar.svelte
+++ src/UserBar.svelte
@@ -8,30 +8,31 @@
   const logout = getContext('logout')
 
   export let mini = false
-  export let generic = false
 </script>
-
 
 <FB c="user-bar" fw={7}>
   {#if $hassess}
     {#if $session.val.login}
       <Link nst="CPB:user">
-        {#if generic}
+        {#if mini}
           USER
         {:else}
           {$session.val.handle}
         {/if}
       </Link>
     {:else if !$rc.singleuser}
-      <span>{#if !generic}{$session.val.handle}{/if}</span>
+      {#if !mini}<span>{$session.val.handle}</span>{/if}
     {/if}
   {/if}
-  {#if mini}<FB>/</FB>{/if}
+
+  {#if mini && $hassess && $session.val.login}<FB vc expand c="ubmini fill"/>{/if}
+
   {#if $hassess && $session.val.login}
     <Link first={logout} global self info="End your session. We'll be sorry to see you go!">LOGOUT</Link>
   {:else if !$rc.singleuser}
     <Link nst="CPB:login">LOGIN</Link>
+    {#if mini && $hassess && !$session.val.login}<FB vc expand c="ubmini fill"/>{/if}
     <Link nst="CPB:register">REGISTER</Link>
   {/if}
-  {#if mini}<FB expand c="ubmini fill"/>{/if}
+
 </FB>
