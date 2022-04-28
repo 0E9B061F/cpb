a9ed52bf52589d1f99e7cb3edd95de7bf39202f2
aetherised <aetherised@gmail.com>
2022-04-28 16:37:10 -0400

    adjust user bar

--- src/UserBar.svelte
+++ src/UserBar.svelte
@@ -10,29 +10,32 @@
   export let mini = false
 </script>
 
-<FB c="user-bar" fw={7}>
-  {#if $hassess}
-    {#if $session.val.login}
-      <Link nst="CPB:user">
-        {#if mini}
-          USER
-        {:else}
-          {$session.val.handle}
-        {/if}
-      </Link>
+<FB c="user-bar" fw={7} line="s2">
+  {#if $rc.singleuser}
+    <FB expand c="fill"/>
+  {:else}
+    {#if $hassess}
+      {#if $session.val.login}
+        <Link nst="CPB:user">
+          {#if mini}
+            USER
+          {:else}
+            {$session.val.handle}
+          {/if}
+        </Link>
+      {:else if !$rc.singleuser}
+        {#if !mini}<span>{$session.val.handle}</span>{/if}
+      {/if}
+    {/if}
+
+    {#if mini && $hassess && $session.val.login}<FB vc expand c="ubmini fill"/>{/if}
+
+    {#if $hassess && $session.val.login}
+      <Link first={logout} global self info="End your session. We'll be sorry to see you go!">LOGOUT</Link>
     {:else if !$rc.singleuser}
-      {#if !mini}<span>{$session.val.handle}</span>{/if}
+      <Link nst="CPB:login">LOGIN</Link>
+      {#if mini && $hassess && !$session.val.login}<FB vc expand c="ubmini fill"/>{/if}
+      <Link nst="CPB:register">REGISTER</Link>
     {/if}
   {/if}
-
-  {#if mini && $hassess && $session.val.login}<FB vc expand c="ubmini fill"/>{/if}
-
-  {#if $hassess && $session.val.login}
-    <Link first={logout} global self info="End your session. We'll be sorry to see you go!">LOGOUT</Link>
-  {:else if !$rc.singleuser}
-    <Link nst="CPB:login">LOGIN</Link>
-    {#if mini && $hassess && !$session.val.login}<FB vc expand c="ubmini fill"/>{/if}
-    <Link nst="CPB:register">REGISTER</Link>
-  {/if}
-
 </FB>
